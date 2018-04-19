8c8edf94c89c0221a7d6de40f0bb5c64c8fe4fd5
cqyisbug <cqyisbug@163.com>
2018-04-19 15:52:02 +0800

    [M] fix path bugs in delete mode

--- filemonitor.py
+++ filemonitor.py
@@ -1,14 +1,14 @@
 # !/usr/bin/env python
 # -*- coding:UTF-8 -*-
-# python version 3.5
-
+# please use python3
+# author caiqyxyx
 
 import os
 import queue
 import hashlib
 import time
 
-#         Constant
+# Constant
 QUE_MAX = 999999
 
 global q
@@ -21,27 +21,20 @@ file_map = {}
 file_set = set([])
 
 
-
 def monitor(path):
     global file_set
     if os.path.isdir(path):
+        new_file_set = set([])
         for root, dirs, files in os.walk(path):
-            new_file_set = set(files)
-            diff = file_set.difference(new_file_set)
-            if len(diff) > 0 :
-                for f in diff:
-                    print("{0} has been deleted".format(os.path.join(root, f)))
-                    file_map.popitem(hash(os.path.join(root, f)))
-                    delete_q.put(os.path.join(root,f))
-                    file_set = new_file_set
             for f in files:
+                new_file_set = new_file_set | set([os.path.join(root, f)])
                 if hash(os.path.join(root, f)) in file_map:
                     if file_map[hash(os.path.join(root, f))] == md5(os.path.join(root, f)):
                         pass
                     else:
                         print("{0} has been modified".format(
                             os.path.join(root, f)))
-                        file_map[hash(os.path.join(root, f))]=md5(
+                        file_map[hash(os.path.join(root, f))] = md5(
                             os.path.join(root, f))
                         q.put(os.path.join(root, f))
                 else:
@@ -49,11 +42,18 @@ def monitor(path):
                     file_map.setdefault(
                         hash(os.path.join(root, f)), md5(os.path.join(root, f)))
                     q.put(os.path.join(root, f))
+        diff = file_set.difference(new_file_set)
+        file_set = new_file_set
+        if len(diff) > 0:
+            for f in diff:
+                print("{0} has been deleted".format(f))
+                file_map.pop(hash(f))
+                delete_q.put(f)
 
 
 def md5(file):
-    md5file=open(file, 'rb')
-    md5_=hashlib.md5(md5file.read()).hexdigest()
+    md5file = open(file, 'rb')
+    md5_ = hashlib.md5(md5file.read()).hexdigest()
     md5file.close()
     return md5_
 
